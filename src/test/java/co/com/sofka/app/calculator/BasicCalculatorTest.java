692d9882d36e0db971001424264296edce5d508a
gabrielfva <gabrielfva@gmail.com>
2021-02-18 16:04:42 -0500

    Se agregaron múltiples casos de entrada. Se agregó plugin maven-assembly

--- src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
+++ src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
@@ -1,7 +1,10 @@
 package co.com.sofka.app.calculator;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static junit.framework.Assert.assertEquals;
 
@@ -21,5 +24,19 @@ public class BasicCalculatorTest {
 
         // Assert
         assertEquals(expectedValue, result);
+    }
+
+    @DisplayName("Testing several sums")
+    @ParameterizedTest(name = "{0} + {1} = {2}")
+    @CsvSource({
+            "0,    1,   1",
+            "1,    2,   3",
+            "49,  51, 100",
+            "1,  100, 101"
+    })
+
+    public void severalSums(Long first, Long second, Long expectedResult) {
+        Assertions.assertEquals(expectedResult, basicCalculator.sum(first, second),
+                () -> first + " + " + second + " should equal " + expectedResult);
     }
 }
