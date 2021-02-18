4cbb63d71c7736151f2fbb851d37088ce8ac646b
gabrielfva <gabrielfva@gmail.com>
2021-02-18 16:30:44 -0500

    Se solucionó TODO: Hacer todos los escenarios restantes para la verificación de pruebas

--- src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
+++ src/test/java/co/com/sofka/app/calculator/BasicCalculatorTest.java
@@ -39,4 +39,43 @@ public class BasicCalculatorTest {
         Assertions.assertEquals(expectedResult, basicCalculator.sum(first, second),
                 () -> first + " + " + second + " should equal " + expectedResult);
     }
+
+    @DisplayName("Testing several rests")
+    @ParameterizedTest(name = "{0} - {1} = {2}")
+    @CsvSource({
+            "0,      2,   -2",
+            "10,     5,    5",
+            "49,    40,    9",
+            "100, -100,  200"
+    })
+    void severalRests(Long first, Long second, Long expectedResult) {
+        Assertions.assertEquals(expectedResult, basicCalculator.sub(first, second),
+                () -> first + " - " + second + " should equal " + expectedResult);
+    }
+
+    @DisplayName("Testing several mults")
+    @ParameterizedTest(name = "{0} * {1} = {2}")
+    @CsvSource({
+            "0,       9,      0",
+            "5,       2,     10",
+            "49,     35,   1715",
+            "100,  -500, -50000"
+    })
+    void severalMults(Long first, Long second, Long expectedResult) {
+        Assertions.assertEquals(expectedResult, basicCalculator.mul(first, second),
+                () -> first + " * " + second + " should equal " + expectedResult);
+    }
+
+    @DisplayName("Testing several divss")
+    @ParameterizedTest(name = "{0} / {1} = {2}")
+    @CsvSource({
+            "0,       5,    0",
+            "10,      0,    0",
+            "25,      5,    5",
+            "100,  -200,   -0"
+    })
+    void severalDivs(Long first, Long second, Long expectedResult) {
+        Assertions.assertEquals(expectedResult, basicCalculator.div(first, second),
+                () -> first + " / " + second + " should equal " + expectedResult);
+    }
 }
